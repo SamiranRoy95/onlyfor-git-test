a262162df77866e63bfa9e1552ee4e62b1c83403
SamiranRoy95 <somironroy80@gmail.com>
2024-01-12 17:40:04 +0600

    test commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Home from './pages/Home/Home'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <Home/>
+        <h1>This is a vite app</h1>
+      </div>
+        
+      
+      
+    </>
+  )
+}
+
+export default App
